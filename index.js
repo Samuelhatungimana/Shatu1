77ffe82185e6f6b26e7c919ea031644f2a514f2d
Samuelhatungimana <samuelhatungimana67@gmail.com>
2025-11-06 14:25:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*** You will not need this file until Unit 5 ***/
+let themebutton = document.getElementById("theme-button")
+const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+    
+}
+ themebutton.addEventListener("click", toggleDarkMode);
+
+
+
+/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
+/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
